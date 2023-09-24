d85bba436287e3afe14d2248b6ec042f5a8babea
Hung <longvanleu69@gmail.com>
2023-09-24 11:00:51 +0700

    add forgot password

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
 import SignInSide from './components/SignInSide';
+import ForgotPassword from './components/ForgotPassword';
+import SuccessResetPassword from './components/SuccessResetPassword'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <SignInSide/>
+        <SuccessResetPassword/>
       </div>
     </div>
   );
