7e5925ae29f40393776e533b1bf9712e7fab679f
pikaden <87664605+pikaden@users.noreply.github.com>
2023-10-25 11:31:10 +0700

    update search product by keyword

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -8,18 +8,18 @@ const Search = () => {
     const search = useContext(SearchContext)
     const [ searchParam, setSearchParam ] = useSearchParams()
 
-    const searchQuery = {
-        query: search.searchQuery
+    const searchKeyword = {
+        keyword: search.searchKeyword
     }
 
     useEffect(() => {
-        setSearchParam(searchQuery, { replace: true })
-    }, [searchQuery.query])
+        setSearchParam(searchKeyword, { replace: true })
+    }, [searchKeyword.keyword])
 
     return ( 
         <div className="search__container">
             <div className="search__container__header">
-                <h1>No results found for "{search.searchQuery}"</h1>
+                <h1>No results found for "{search.searchKeyword}"</h1>
             </div>
         </div>
      );
--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -2,8 +2,6 @@ import { useParams } from "react-router-dom";
 import Search from "../components/Search";
 
 const SearchView = () => {
-    const param = useParams()
-    console.log(param.query)
     return ( 
         <Search />
      );
