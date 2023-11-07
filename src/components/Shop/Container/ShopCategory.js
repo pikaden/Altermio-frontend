43464fa1014a26c147fa82fcc9c9e4af50b4d1c8
Thanh <hondondaide@gmail.com>
2023-11-07 15:48:25 +0700

    update AppBar final

--- src/components/Shop/Container/ShopCategory.js
+++ src/components/Shop/Container/ShopCategory.js
@@ -1,24 +1,50 @@
+
+import { useEffect, useState } from "react";
 import ItemCard from "../../Card/ItemCard/ItemCard";
 import './ShopCategory.css'
+import axios from "axios";
+import ReactLoading from 'react-loading';
+import { Link } from "react-router-dom";
 
 const ShopCategory = (props) => {
-    return ( 
+    const [items, setItems] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const fetchProductListById = async () => {
+        await axios.get(`http://localhost:3000/v1/productLists/manage/${props.category.id}`)
+            .then(res => setItems(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetchProductListById()
+        setLoading(false)
+    }, [])
+
+    return (
         <div className="shop__category__container">
+            {loading && <ReactLoading type="balls" color='#FFE26E' height={100} width={100} className='container h-100 w-10 justify-self-center align-self-center m-auto' />}
+
             <div className="shop__category__header">
                 <div className="shop__category__header__big">
                     <div className="shop__category__head">
-                        <h2>{props.name} Fashion</h2>
-                    </div> 
+                        <Link to={`/category/${props.category.id}`} >
+                            <h2>{props.category.categoryName}</h2>
+                        </Link>
+                    </div>
                     <div className="shop__category__header__line"></div>
                 </div>
-                </div>
+            </div>
+
+            {items &&
                 <div className="shop__category__card__container">
                     <div className="shop__category__product__card">
-                        {props.items.map((data) => <ItemCard item={data} category={props.category}/>)}
+                        {items.results.products.map((data) => <ItemCard item={data} />)}
                     </div>
-            </div>
+                </div>
+            }
         </div>
-     );
+    );
 }
- 
+
 export default ShopCategory;
